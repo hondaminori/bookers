dd773dda47ad21bb78cc0e3e225c81598a48b50b
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-03-25 03:25:52 +0900

    (17)createが成功したとき

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id), notice: 'Book was successfully created.'
   end
 
   def index
