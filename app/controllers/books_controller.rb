e92acdc079ce392b8f83ba9c45b48995f9afa71e
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-03-26 12:39:33 +0900

    (21)RSpecのテストファイル準備

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,7 +28,6 @@ class BooksController < ApplicationController
   end
   
   def update
-    byebug
     book = Book.find(params[:id])
     if book.update(book_params)
       redirect_to book_path(book.id), notice: 'Book was successfully updated.'
